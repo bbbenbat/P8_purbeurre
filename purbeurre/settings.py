e7fcb1cb76c94a5187eaedd1408511c8abaf5a45
bbbenbat <bbessayah@gmail.com>
2021-03-07 17:58:51 +0100

    heroku prepa2

--- purbeurre/settings.py
+++ purbeurre/settings.py
@@ -153,7 +153,7 @@ STATICFILES_DIRS = (
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 SECRET_KEY = config('SECRET_KEY')
-DEBUG = config('DEBUG', default=False, cast=bool)
+DEBUG = config('DEBUG', default=True, cast=bool)
 DATABASES = {
     'default': dj_database_url.config(
         default=config('DATABASE_URL')
