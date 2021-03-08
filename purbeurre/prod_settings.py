d8e4631580d6a20e7bfb91103bf52871d457b361
bbbenbat <bbessayah@gmail.com>
2021-03-08 12:53:25 +0100

    update db prod

--- purbeurre/prod_settings.py
+++ purbeurre/prod_settings.py
@@ -1,12 +1,14 @@
+import dj_database_url
+
 from purbeurre.settings import *
 
 DEBUG = False
 TEMPLATE_DEBUG = False
 
+DATABASES['default'] = dj_database_url.config()
+
 SECRET_KEY = '&6@7a)no4f^o(o3_2ta^)*5zu82p!^)guvu5v3b^1be+a)%(yw'
 
 ALLOWED_HOSTS = ['purbeurre-ocr.herokuapp.com']
-
-
 
 
