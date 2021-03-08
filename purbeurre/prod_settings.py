3b9d147eaf83905adea54684c58eb84bdd005fdb
bbbenbat <bbessayah@gmail.com>
2021-03-08 13:44:26 +0100

    whitenoise

--- purbeurre/prod_settings.py
+++ purbeurre/prod_settings.py
@@ -8,7 +8,7 @@ TEMPLATE_DEBUG = False
 DATABASES['default'] = dj_database_url.config()
 
 MIDDLEWARE += ['whitenoise.middleware.WhiteNoiseMiddleware']
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 SECRET_KEY = '&6@7a)no4f^o(o3_2ta^)*5zu82p!^)guvu5v3b^1be+a)%(yw'
 
