bb7bda1b3e4af84f5a55765e2e37d48759af3831
bbbenbat <bbessayah@gmail.com>
2021-03-08 12:43:40 +0100

    url

--- purbeurre/prod_settings.py
+++ purbeurre/prod_settings.py
@@ -5,7 +5,7 @@ TEMPLATE_DEBUG = False
 
 SECRET_KEY = '&6@7a)no4f^o(o3_2ta^)*5zu82p!^)guvu5v3b^1be+a)%(yw'
 
-ALLOWED_HOSTS = ['ocr-purbeurre.herokuapp.com']
+ALLOWED_HOSTS = ['purbeurre-ocr.herokuapp.com']
 
 
 
