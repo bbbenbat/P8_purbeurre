59e1f4e7baea8a8ed86b1dfb59cc2d18fc08ea38
bbbenbat <bbessayah@gmail.com>
2021-03-08 12:14:08 +0100

    responsive

--- purbeurre/settings.py
+++ purbeurre/settings.py
@@ -133,6 +133,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # new
 STATIC_URL = '/static/'  # new
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')  # new
 MEDIA_URL = '/media/'  # new
