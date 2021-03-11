3092ba1231abc0a360a9c811b60b8117ef3e9372
bbbenbat <bbessayah@gmail.com>
2021-03-11 23:24:20 +0100

    update start_db.py

--- researches/start_db.py
+++ researches/start_db.py
@@ -13,6 +13,6 @@ from researches.controllers import save_data_api
 
 api_off = openfoodfacts_api.ApiOff()
 
-product = api_off.api_connection(3, 400)
+product = api_off.api_connection(5, 300)
 data_db = save_data_api.SaveDataApi()
 data_db.save_products(product)
