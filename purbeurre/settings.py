89e58ea219e237a024c129f720f07ace69c26431
bbbenbat <bbessayah@gmail.com>
2022-02-19 18:29:16 +0100

    update env var settings

--- purbeurre/settings.py
+++ purbeurre/settings.py
@@ -34,9 +34,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = get_env_variable('SECRET_KEY',
-                              '%tyzr%74fp$(o5y3s%9+#4177++qzd-mmbnlnb&b__p('
-                              '=nx3-b')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
