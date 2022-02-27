d9a8babd4a397e79434435c6faf039a3392dd611
bbbenbat <bbessayah@gmail.com>
2022-02-27 14:45:16 +0100

    update prod_settings

---
+++ purbeurre/prod_settings.py
@@ -0,0 +1,13 @@
+import dj_database_url
+
+from purbeurre.settings import *
+
+DEBUG = False
+TEMPLATE_DEBUG = False
+
+DATABASES['default'] = dj_database_url.config()
+
+MIDDLEWARE += ['whitenoise.middleware.WhiteNoiseMiddleware']
+
+ALLOWED_HOSTS = ['purbeurre8-ocr.herokuapp.com']
+
